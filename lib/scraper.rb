5445c00306ef51da9d9eea7894b78087881cb0cb
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-20 20:58:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,35 +6,30 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    # doc.css(".post").each do |post|
-    #   course = Course.new
-    #   course.title = doc.css(".post").first.css("h2").text
-    #   course.schedule = doc.css(".post").first.css(".date").text
-    #   course.description = doc.css(".post").first.css("p").text
-    # end
-    # doc.css(".post") << collection of courses
-    # doc.css(".post").first.css("h2").text << title
-    # doc.css(".post").first.css(".date").text << schedule
-    # doc.css(".post").first.css("p").text << description
-    # binding.pry
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
   end 
   
   def get_courses
+    
     self.get_page.css(".post")
+    
   end 
   
   def make_courses 
+    
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
     end
+    
   end
   
   def print_courses
+    
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
@@ -43,6 +38,7 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+    
   end
   
 end
